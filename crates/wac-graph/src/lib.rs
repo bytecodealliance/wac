40b3c38bbdccc4d3ccc8e72b18fc817ea08b87d0
Peter Huene <peter@huene.dev>
2024-04-03 23:27:43 -0700

    Add a code example to the `wac-graph` crate.

--- crates/wac-graph/src/lib.rs
+++ crates/wac-graph/src/lib.rs
@@ -1,4 +1,41 @@
 //! A library for defining, encoding, and decoding WebAssembly composition graphs.
+//!
+//! An example of composing two components together using a `CompositionGraph`:
+//!
+//! ```rust,no_run
+//! use wac_graph::{CompositionGraph, EncodeOptions, types::Package};
+//!
+//! # fn main() -> anyhow::Result<()> {
+//! let mut graph = CompositionGraph::new();
+//!
+//! // Register the packages with the graph
+//! // It is assumed that `my:package1` exports a function named `a`,
+//! // while `my:package2` imports a function named `b`.
+//! let package1 = graph.register_package(
+//!     Package::from_file("my:package1", None, "package1.wasm")?
+//! )?;
+//! let package2 = graph.register_package(
+//!     Package::from_file("my:package2", None, "package2.wasm")?
+//! )?;
+//!
+//! // Instantiate package `my:package1`
+//! let instantiation1 = graph.instantiate(package1)?;
+//!
+//! // Alias the `a` export of the `my:package1` instance
+//! let a = graph.alias_instance_export(instantiation1, "a")?;
+//!
+//! // Instantiate package `my:package2`
+//! let instantiation2 = graph.instantiate(package2)?;
+//!
+//! // Connect `a` to the argument `b` of the instantiation
+//! graph.connect_argument(a, instantiation2, "b")?;
+//!
+//! // Finally, encode the graph into a new component
+//! let bytes = graph.encode(EncodeOptions::default())?;
+//!
+//! # Ok(())
+//! # }
+//! ````
 
 #![deny(missing_docs)]
 
