1096a5be156c93f04993bfd908d600806d55960d
Calvin Prewitt <calvin@JafLabs.com>
2024-07-10 08:25:45 -0500

    set version 0.5.0-dev (#131)

--- crates/wac-resolver/tests/registry.rs
+++ crates/wac-resolver/tests/registry.rs
@@ -113,7 +113,7 @@ export i2.foo as "bar";
   (export (;6;) "test:wit/foo" (instance 4))
   (export (;7;) "bar" (instance 5))
   (@producers
-    (processed-by "wac-parser" "0.4.0-dev")
+    (processed-by "wac-parser" "0.5.0-dev")
   )
 )
 "#
