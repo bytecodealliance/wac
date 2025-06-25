3e5750d6571b3448508f42fd8d53c960ad561298
Calvin Prewitt <calvin@jaflabs.com>
2025-06-25 13:54:19 -0500

    Revert "removed no-verify arg"
    This reverts commit b9e042cdf53abf477de6227f2458bc1e4b02f1a3.

--- ci/publish.rs
+++ ci/publish.rs
@@ -303,6 +303,7 @@ fn publish(krate: &Crate) -> bool {
     let status = Command::new("cargo")
         .arg("publish")
         .current_dir(krate.manifest.parent().unwrap())
+        .arg("--no-verify")
         .status()
         .expect("failed to run cargo");
     if !status.success() {
